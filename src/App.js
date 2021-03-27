def9aaca10d98396c869653c83852d0affbddd3b
glolorun <63762681+glolorun@users.noreply.github.com>
2021-03-27 00:37:42 -0400

    imcomplete within recommended timeframe

--- src/App.js
+++ src/App.js
@@ -1,31 +1,44 @@
-import React, {useState } from 'react'
-import './styles.css'
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-export default function App() {
-    const [data , setData]=useState(null)
-    fetch('./masterclass.json').then(
-        function (res) {
-            return res.json()
-        }
-    ).then(function (data) {
-        setData(data)
-    }).catch(function(err){console.log(err, 'error')})
-        
-    return (
-        <div className='App'>
-            {
-                data? data.map(
-          function(data){
-                  return (<div className="card"> 
-                  <h4> {data.title}</h4>
-                  <h2> {data.description}  {data.lastName}</h2>
-                  <h3> {data.instructor_name}</h3>
-                  <img src={data.instructor_image_url} alt={data.instructor_name}/>
-              </div>)
-          }
-        ):""
-      }
-        </div>
-    );
-    }
+import "./styles.css";
+// import { Fclike, FcLikePlaceholder } from 'react-icons/fa';
 
+const App = () => {
+  const [data, setData] = useState(null);
+  fetch("./masterclass.json")
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      setData(data);
+    })
+    .catch(function (err) {
+      console.log(err, "error");
+    });
+
+
+    
+  return (
+    <div className="App">
+      {data
+        ? data.map(function (data) {
+            return (
+              <div className="">
+                <img
+                  src={data.instructor_image_url}
+                  alt={data.instructor_name}
+                />
+                <h1> {data.instructor_name}</h1>
+                <h4> {data.title}</h4>
+                <h2>
+                  {data.description}
+                </h2>
+              </div>
+            );
+          })
+        : ""}
+    </div>
+  );
+};
+export default App;
